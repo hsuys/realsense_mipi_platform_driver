bf217ad6b808176e828fcd815897e0742456c7bb
Yusheng Hsu <yusheng.hsu@intel.com>
2025-06-27 18:05:52 -0700

    Revert PR#242, Commit:ce601bc

--- kernel/realsense/d4xx.c
+++ kernel/realsense/d4xx.c
@@ -4973,8 +4973,10 @@ static int ds5_dfu_device_open(struct inode *inode, struct file *file)
 	struct ds5 *state = container_of(inode->i_cdev, struct ds5,
 			dfu_dev.ds5_cdev);
 #ifdef CONFIG_TEGRA_CAMERA_PLATFORM
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 10)
 	struct i2c_adapter *parent = i2c_parent_is_i2c_adapter(
 			state->client->adapter);
+#endif
 #endif
 	mutex_lock(&state->lock);
 	if (state->dfu_dev.device_open_count) {
@@ -4992,6 +4994,7 @@ static int ds5_dfu_device_open(struct inode *inode, struct file *file)
 	}
 	file->private_data = state;
 #ifdef CONFIG_TEGRA_CAMERA_PLATFORM
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 10)
 	/* get i2c controller and set dfu bus clock rate */
 	while (parent && i2c_parent_is_i2c_adapter(parent))
 		parent = i2c_parent_is_i2c_adapter(state->client->adapter);
@@ -5008,6 +5011,7 @@ static int ds5_dfu_device_open(struct inode *inode, struct file *file)
 
 	state->dfu_dev.bus_clk_rate = i2c_get_adapter_bus_clk_rate(parent);
 	i2c_set_adapter_bus_clk_rate(parent, DFU_I2C_BUS_CLK_RATE);
+#endif
 #endif
 	mutex_unlock(&state->lock);
 	return 0;
@@ -5067,8 +5071,10 @@ static int ds5_dfu_device_release(struct inode *inode, struct file *file)
 {
 	struct ds5 *state = container_of(inode->i_cdev, struct ds5, dfu_dev.ds5_cdev);
 #ifdef CONFIG_TEGRA_CAMERA_PLATFORM
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 10)
 	struct i2c_adapter *parent = i2c_parent_is_i2c_adapter(
 			state->client->adapter);
+#endif
 #endif
 	int ret = 0, retry = 10;
 	mutex_lock(&state->lock);
@@ -5085,6 +5091,7 @@ static int ds5_dfu_device_release(struct inode *inode, struct file *file)
 		devm_kfree(&state->client->dev, state->dfu_dev.dfu_msg);
 	state->dfu_dev.dfu_msg = NULL;
 #ifdef CONFIG_TEGRA_CAMERA_PLATFORM
+#if LINUX_VERSION_CODE < KERNEL_VERSION(5, 15, 10)
 	/* get i2c controller and restore bus clock rate */
 	while (parent && i2c_parent_is_i2c_adapter(parent))
 		parent = i2c_parent_is_i2c_adapter(state->client->adapter);
@@ -5098,6 +5105,7 @@ static int ds5_dfu_device_release(struct inode *inode, struct file *file)
 			state->dfu_dev.bus_clk_rate);
 
 	i2c_set_adapter_bus_clk_rate(parent, state->dfu_dev.bus_clk_rate);
+#endif
 #endif
 	/* Verify communication */
 	do {
